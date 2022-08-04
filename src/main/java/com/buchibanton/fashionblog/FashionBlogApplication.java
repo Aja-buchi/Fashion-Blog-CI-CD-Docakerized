571b61fea40577fd9b452a21d231b5b53aa2da12
Aja-Buchi <aja.onyebuchi@yahoo.com>
2022-08-04 15:26:50 +0100

    Includes docker-compose.yml file

--- src/main/java/com/buchibanton/fashionblog/FashionBlogApplication.java
+++ src/main/java/com/buchibanton/fashionblog/FashionBlogApplication.java
@@ -9,12 +9,4 @@ public class FashionBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(FashionBlogApplication.class, args);
     }
-
-//    echo "# Fashion-Blog-CI-CD-Docakerized" >> README.md
-//    git init
-//    git add README.md
-//    git commit -m "first commit"
-//    git branch -M main
-//    git remote add origin https://github.com/Aja-buchi/Fashion-Blog-CI-CD-Docakerized.git
-//    git push -u origin main
 }
